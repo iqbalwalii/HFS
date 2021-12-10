bb4af3ff3f6e3e839a37f4ccdc64ceddfe58aef7
iqbalwalii <iqbalwali786@gmail.com>
2021-12-10 12:31:04 +0530

    order detail in dashboard

--- src/pages/admin/update/[id].js
+++ src/pages/admin/update/[id].js
@@ -1,24 +1,113 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col, Button, ListGroup } from "react-bootstrap";
+import { Table, Container, Row, Col, Button, ListGroup } from "react-bootstrap";
 import Axios from "../../../utils/axios";
+import Image from "next/image";
 const orderDetail = (props) => {
+  const [items, setItems] = useState({});
   useEffect(() => {
     async function fetchOrder(id) {
       const { data } = await Axios.get(`/api/orders/${id}`);
-      console.log("Maaaaaaaaaaaaaaaaal", data);
+      setItems(data.order);
+      console.log(data.order);
     }
     fetchOrder(props.orderId);
   }, []);
   return (
     <Container>
+      <h3 className="text-center mt-3">Order Details</h3>
+
       <Row>
-        <Col xs={12} md={3}></Col>
-        <Col xs={12} md={9}></Col>
+        <Col xs={12} md={3}>
+          <div className="imageCon">
+            {items?.orderItems?.map((prod) => {
+              return (
+                <Image
+                  src={prod.image}
+                  width="100px"
+                  height="100px"
+                  alt="product image"
+                />
+              );
+            })}
+          </div>
+        </Col>
+        <Col xs={12} md={9} className="mt-4">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Product Name</th>
+                <th>Quantity</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items?.orderItems?.map((prod, index) => {
+                return (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>{prod.name}</td>
+                    <td>{prod.quantity}</td>
+                    <td>{prod.price}</td>
+                  </tr>
+                );
+              })}
+              <tr>
+                <td colspan={3}>
+                  <h6 className="text-end px-5">
+                    <strong>TAX</strong>
+                  </h6>
+                </td>
+                <td>
+                  <strong>{items?.taxAmount}</strong>
+                </td>
+              </tr>
+              <tr>
+                <td colspan={3}>
+                  <h6 className="text-end px-5">
+                    <strong>TOTAL</strong>
+                  </h6>
+                </td>
+                <td>
+                  <strong style={{ color: "#f00" }}>
+                    {items?.totalAmount}
+                  </strong>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <ListGroup>
+            <ListGroup.Item>
+              Delivery Status : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {items?.isDelivered === true ? " Delivered" : "in Transit"}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Payment Status : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {items?.isPaid === true ? " Paid" : "Not Paid"}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Payment Method : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {items?.paymentMethod}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Ordered Date : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {items?.createdAt?.slice(0, 10)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Shipping Address : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {items?.shippingAddress?.address}, {items?.shippingAddress?.city}-
+              {items?.shippingAddress?.postalCode}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
       </Row>
     </Container>
   );
 };
-
 export default orderDetail;
 export async function getServerSideProps({ params }) {
   console.log("orderrrrrrrrr", params);
